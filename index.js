9f347d93949327bc801ba8e03696d7949aae35b7
elkoneaas <37072745+elkoneaas@users.noreply.github.com>
2018-03-16 10:47:31 +0200

    Hash added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello world from Jenkins & Docker world!!!");
+    response.end("Hello world from Jenkins & Docker world!#!");
 });
 
 var port = process.env.PORT || 1337;
